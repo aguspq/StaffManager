4b0f7ad55fd44dbb20908c8fb81bc04569bd92f2
Agus <agustin.pq@icloud.com>
2026-09-18 14:34:32 +0200

    refactor(auth): fix incorrect file location for AuthController

--- src/main/java/com/agus/springboot/controllers/AuthController.java
+++ src/main/java/com/agus/springboot/controllers/AuthController.java
@@ -1,4 +1,28 @@
 package com.agus.springboot.controllers;
 
+import com.agus.springboot.dto.AuthLoginRequest;
+import com.agus.springboot.dto.AuthResponse;
+import com.agus.springboot.service.impl.UserDetailServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final UserDetailServiceImpl userDetailService;
+
+    public AuthController(UserDetailServiceImpl userDetailService) {
+        this.userDetailService = userDetailService;
+    }
+
+    @PostMapping("/log-in")
+    public ResponseEntity<AuthResponse> login(@RequestBody @Valid AuthLoginRequest userRequest) {
+        return new ResponseEntity<>(this.userDetailService.loginUser(userRequest), HttpStatus.OK);
+    }
 }
